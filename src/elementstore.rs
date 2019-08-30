ed543184377f8af552d36e719beb2639af3be874
Maarten van Gompel <proycon@anaproy.nl>
2019-08-30 23:14:16 +0200

    cleanup

--- src/elementstore.rs
+++ src/elementstore.rs
@@ -28,62 +28,6 @@ impl Store<FoliaElement> for ElementStore {
 }
 
 impl ElementStore {
-    /*
-    pub fn add(&mut self, element: FoliaElement) -> IntId {
-        let id = element.id();
-        let boxed = Box::new(element);
-        self.elements.push( Some(boxed) );
-        let intid = self.elements.len() - 1;
-        if let Some(id) = id {
-            self.index.insert(id, intid);
-        }
-        intid
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.elements.is_empty()
-    }
-
-    pub fn len(&self) -> usize {
-        self.elements.len()
-    }
-
-    ///Retrieves an element from the store
-    pub fn get(&self, intid: IntId) -> Option<&Box<FoliaElement>> {
-        if let Some(intid) = self.elements.get(intid) { //-> Option<&Option<Box<T>>>
-            intid.as_ref()
-        } else {
-            None
-        }
-    }
-
-    ///Retrieves an element from the store
-    pub fn get_mut(&mut self, intid: IntId) -> Option<&mut Box<FoliaElement>> {
-        if let Some(intid) = self.elements.get_mut(intid) { //-> Option<&Option<Box<T>>>
-            intid.as_mut()
-        } else {
-            None
-        }
-    }
-
-    ///Resolve an ID to an Internal ID using the index
-    pub fn id(&self, id: &str) -> Option<IntId> {
-        self.index.get(id).map( |&intid| intid )
-    }
-
-    pub fn get_by_id(&self, id: &str) -> Option<&Box<FoliaElement>> {
-        self.id(id).map( |intid| {
-            self.get(intid)
-        }).map(|o| o.unwrap())
-    }
-
-    pub fn get_mut_by_id(&mut self, id: &str) -> Option<&mut Box<FoliaElement>> {
-        self.id(id).map( move |intid| {
-            self.get_mut(intid)
-        }).map(|o| o.unwrap())
-    }
-    */
-
     ///Adds an element as a child on another, this is a higher-level function that/
     ///takes care of adding and attaching for you.
     pub fn add_to(&mut self, parent_intid: IntId, child: FoliaElement) -> IntId {
